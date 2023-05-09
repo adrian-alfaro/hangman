4c28abb5d90ffb106873a0af9853731d80ba5f32
adrian-alfaro <adrianalfaro97@gmail.com>
2023-05-08 20:43:04 -0600

    the interactive part is in progress

--- hangman.py
+++ hangman.py
@@ -15,4 +15,16 @@ spaces_word = '_ '*len(current_word)
 print(current_word)
 print(spaces_word)
 
+current_guess = input("Please enter one letter: ")
 
+while current_guess in current_word:
+    print("nice!")
+    index = (current_word.find(current_guess))
+    if index != -1:
+        spaces_word[index+index] = current_guess
+        current_word[index] = "_"
+
+
+def replacer():
+    while index:
+        pass

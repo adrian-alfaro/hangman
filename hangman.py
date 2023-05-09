e9d4493fe1c2d2de3b22ac05b2c0fe932ec11de8
adrian-alfaro <adrianalfaro97@gmail.com>
2023-05-08 19:44:18 -0600

    now the program is able to select a random word from a list

--- hangman.py
+++ hangman.py
@@ -1,3 +1,12 @@
 # This is a hangman game
 
 # Author: Adrian Alfaro
+# StartDate: 08/05/2023
+
+words = ['cookie', 'pizza', 'hamburger', 'chicken']
+
+import random as rd
+
+current_word = words[rd.randint(0,len(words))]
+
+print(current_word)

1b8bd63e474b11dbdadb0fcee4b18623c29e8163
adrian-alfaro <adrianalfaro97@gmail.com>
2023-05-08 20:11:36 -0600

    now we have a string that shows the spaces of the word

--- hangman.py
+++ hangman.py
@@ -7,6 +7,12 @@ words = ['cookie', 'pizza', 'hamburger', 'chicken']
 
 import random as rd
 
-current_word = words[rd.randint(0,len(words))]
+# Selects a random word and assigns it to a variable
+current_word = words[rd.randint(0,len(words)-1)]
+# Creates a string of underscores, one per letter
+spaces_word = '_ '*len(current_word)
 
 print(current_word)
+print(spaces_word)
+
+

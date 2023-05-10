568127f7f93a78c06e73a21e0f2afbfefe79bae8
adrian-alfaro <adrianalfaro97@gmail.com>
2023-05-09 19:36:33 -0600

    saving some progress on the GUI

--- graphical-elements.py
+++ graphical-elements.py
@@ -1,13 +1,33 @@
 # here I want to test the more graphical aspects of the game
 
+#    Remaining lives: 3 
 #     ________
-#    |        |
-#    |    ( )_|
-#    |    /|\   
-#    |   | | | 
-#    |    / \
+#    |        |     A B C D E  
+#    |    ( )_|     F G H I J 
+#    |    /|\       K L M N O  
+#    |   | | |      P Q R S T  
+#    |    / \       U V W X Y Z
 #    |   /   \ 
 #    | 
 #    |__________
+#
+#    P I Z Z _ 
+
+hang_3 = (' '*4 + '_'*8 + '\n'
+          + ' '*3 + '|' + ' '*8 + '|')
+
+hang_4 = ('    ________' + '\n' + '   |        |    Remaining lives: 3' + '\n' + '   |    ( )_|' + '\n' + 
+          '   |    /|\      A B C D E' + '\n' + '   |   | | |     F G H I J' + '\n' + '   |    / \  ')
+
+print(hang_4)
 
 
+#     ________
+#    |        |       
+#    |    ( )_|     
+#    |    /|\        
+#    |   | | |       
+#    |    / \       
+#    |   /   \ 
+#    | 
+#    |__________

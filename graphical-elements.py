a64e734c1d35032f92dc5a5f44e12860bbf95e3e
adrian-alfaro <adrianalfaro97@gmail.com>
2023-05-09 19:08:13 -0600

    added a file that will contain the graphical elements

---
+++ graphical-elements.py
@@ -0,0 +1,13 @@
+# here I want to test the more graphical aspects of the game
+
+#     ________
+#    |        |
+#    |    ( )_|
+#    |    /|\   
+#    |   | | | 
+#    |    / \
+#    |   /   \ 
+#    | 
+#    |__________
+
+

e26917d767aa87836ac3e480d50051259b9bc802
adrian-alfaro <adrianalfaro97@gmail.com>
2023-05-09 20:12:41 -0600

    the graphical interface is complete, a few improvements may follow

--- graphical-elements.py
+++ graphical-elements.py
@@ -1,25 +1,33 @@
 # here I want to test the more graphical aspects of the game
-
-#    Remaining lives: 3 
+blanks = 'P I Z Z _'
+lifes = 3
+abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+#    This is a reference
 #     ________
-#    |        |     A B C D E  
-#    |    ( )_|     F G H I J 
-#    |    /|\       K L M N O  
-#    |   | | |      P Q R S T  
-#    |    / \       U V W X Y Z
-#    |   /   \ 
-#    | 
+#    |        |     Remaining lifes: 3  
+#    |        |
+#    |    ( )_|     A B C D E 
+#    |    /|\       F G H I J  
+#    |   | | |      K L M N O  
+#    |    / \       P Q R S T 
+#    |   /   \      U V W X Y Z 
 #    |__________
 #
 #    P I Z Z _ 
+#
+# Next letter is:
 
-hang_3 = (' '*4 + '_'*8 + '\n'
-          + ' '*3 + '|' + ' '*8 + '|')
+hang_1 = ('    ________' + '\n' +
+          f'   |        |    Remaining lifes: {lifes}' + '\n' +
+           '   |    ( )_|' + '\n' + 
+          f'   |    /|\      {abc[0] } {abc[1] } {abc[2] } {abc[3] } {abc[4] }' + '\n' + 
+          f'   |   | | |     {abc[5] } {abc[6] } {abc[7] } {abc[8] } {abc[9] }' + '\n' +
+          f'   |    / \      {abc[10]} {abc[11]} {abc[12]} {abc[13]} {abc[14]}' + '\n' +
+          f'   |   /   \     {abc[15]} {abc[16]} {abc[17]} {abc[18]} {abc[19] }' + '\n' +
+          f'   |__________   {abc[20]} {abc[21]} {abc[22]} {abc[23]} {abc[24]} {abc[25]}' +
+          '\n\n' + f'   {blanks}')
 
-hang_4 = ('    ________' + '\n' + '   |        |    Remaining lives: 3' + '\n' + '   |    ( )_|' + '\n' + 
-          '   |    /|\      A B C D E' + '\n' + '   |   | | |     F G H I J' + '\n' + '   |    / \  ')
-
-print(hang_4)
+print(hang_1)
 
 
 #     ________
